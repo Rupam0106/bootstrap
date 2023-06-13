d7a6fd0d8c49fb1ae524cc49f8ce70b0f98644e8
rupam0106 <rupam.gupta@rudrainnovative.in>
2023-06-13 18:49:27 +0530

    ref-populate done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express=require("express");
 const app=express();
 const User=require("./routes/userRoute")
+const Book=require("./routes/bookRoute")
+
 const cors=require("cors")
 //to parse the json data
 app.use(express.json());
 app.use(cors())
 app.use("/api/v1/",User)
+app.use("/api/v1/",Book)
 
 module.exports=app;
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please Provide a name"],
-    minLength: [3,"Please provide minimum 3 Character"],
+    minLength: [3, "Please provide minimum 3 Character"],
     validate: {
       validator: function (name) {
         return /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g.test(name);
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please Provide a Email"],
-    minLength: [5,"Please provide minimum 5 Character"],
+    minLength: [5, "Please provide minimum 5 Character"],
     unique: true,
     validate: {
       validator: function (email) {
@@ -23,13 +23,13 @@ const userSchema = new mongoose.Schema({
           email
         );
       },
-      message: "Please Provide a Valid Email",
+      message: "Please Provide a Valid Email ",
     },
   },
   subject: {
     type: String,
     required: [true, "Please Provide a Valid Subject"],
-    minLength: [5,"Please provide minimum 5 Character"],
+    minLength: [5, "Please provide minimum 5 Character"],
     validate: {
       validator: function (subject) {
         return /^[a-zA-Z.]*$/.test(subject);
@@ -40,7 +40,7 @@ const userSchema = new mongoose.Schema({
   message: {
     type: String,
     required: [true, "Please Provide a details Message"],
-    minLength: [5,"Please provide minimum 5 Character"],
+    minLength: [5, "Please provide minimum 5 Character"],
   },
 });
 
